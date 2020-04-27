283d56ff1a2c326e6c86716af37bc9b9ac79b94d
Raydaydayup <ztrdaydayup@foxmail.com>
2020-04-27 13:43:03 +0800

    json数据动态生成和一半的动态文章列表

--- js/commonCard.js
+++ js/commonCard.js
@@ -2,19 +2,29 @@
  * @Author       : Ray
  * @Date         : 2020-04-24 09:19:29
  * @LastEditors  : Ray
- * @LastEditTime : 2020-04-24 13:33:23
+ * @LastEditTime : 2020-04-27 10:58:11
  * @FilePath     : \myblog\js\commonCard.js
  * @Description  : file content
  */
 (function () {
-	insertPersonCard();
-	insertRecentCard();
-	insertCategoryCard();
-	insertTagsCard();
-	function insertPersonCard() {
+	var xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = function () {
+		if (xhr.readyState == 4 && xhr.status == 200) {
+			const data = JSON.parse(xhr.responseText);
+			const [categoryList, tagsList, articles] = data;
+			insertPersonCard(articles.length, categoryList.length, tagsList.length);
+			insertRecentCard(articles);
+			insertCategoryCard(categoryList);
+			insertTagsCard(tagsList);
+		}
+	};
+	xhr.open("GET", "./data/articles.json", true);
+	xhr.send();
+
+	function insertPersonCard(n1, n2, n3) {
 		return new Promise((resolve, reject) => {
 			const userImg = `(div[class="user-img]>img[src="./favicon.ico])+h2[class="font-regular]{Raydaydayup}+p{👉好好学习，天天向上👈}`;
-			const navStatistics = `nav[class="row nav-statistics]>(a[href="./index.html,class="col-4-sm]>h6[class="font-regular]{文章}+h2[class="font-regular]{16})+(a[href="./categories.html,class="col-4-sm]>h6[class="font-regular]{分类}+h2[class="font-regular]{5})+(a[href="./tags.html,class="col-4-sm]>h6[class="font-regular]{标签}+h2[class="font-regular]{6})`;
+			const navStatistics = `nav[class="row nav-statistics]>(a[href="./index.html,class="col-4-sm]>h6[class="font-regular]{文章}+h2[class="font-regular]{${n1}})+(a[href="./categories.html,class="col-4-sm]>h6[class="font-regular]{分类}+h2[class="font-regular]{${n2}})+(a[href="./tags.html,class="col-4-sm]>h6[class="font-regular]{标签}+h2[class="font-regular]{${n3}})`;
 			const followMe = `a[target="_blank,href="https://github.com/Ray-daydayup,class="row nav-github]>i[class="iconfont icon-github-fill]+span{关注我}`;
 			const navContact = `nav[class="row nav-contact]>(a[href="javascript:;,class="col-4-sm]>i[class="iconfont icon-QQ])+(a[href="javascript:;,class="col-4-sm]>i[class="iconfont icon-wechat-fill])+(a[target="_blank,href="http://mail.qq.com/cgi-bin/qm_share?t=qm_mailme&email=-oSKjJqfh5qfh4uOvpiRhpOfl5LQnZGT,class="col-4-sm]>i[class="iconfont icon-mail-fill])`;
 			const words = `hr+div[class="words]>p[class="font-heavy]{应付生活中各种问题的勇气}+p[class="font-heavy]{能说明一个人如何定义生活的意义}+p{——阿德勒}`;
@@ -26,28 +36,10 @@
 			);
 		});
 	}
-	function insertRecentCard() {
+	function insertRecentCard(recentArticle) {
 		return new Promise((resolve, reject) => {
-			const recentArticle = [
-				{
-					url: "#",
-					title: "Github便是免费且好用的CDN，非常适合博客网站使用",
-				},
-				{
-					url: "#",
-					title: "Github便是免费且好用的CDN",
-				},
-				{
-					url: "#",
-					title: "非常适合博客网站使用",
-				},
-				{
-					url: "#",
-					title: "响应式",
-				},
-			];
 			const articleLink = recentArticle
-				.map((item) => `a[href="${item.url}]{${item.title}}`)
+				.map((item) => `a[href="./detail.html?id=${item.id}]{${item.title}}`)
 				.join("+");
 			const str = `div[class="card-content category-card recent-articles]>h6{最近文章}+(${articleLink})`;
 			element.appendChild(
@@ -57,24 +49,12 @@
 			);
 		});
 	}
-	function insertCategoryCard() {
+	function insertCategoryCard(categoryList) {
 		return new Promise((resolve, reject) => {
-			const categoryList = [
-				{
-					url: "#",
-					title: "前端",
-				},
-				{
-					url: "#",
-					title: "摄影",
-				},
-				{
-					url: "#",
-					title: "心情",
-				},
-			];
 			const categoryLink = categoryList
-				.map((item) => `a[href="${item.url}]{${item.title}}`)
+				.map(
+					(item) => `a[href="./categories.html?id=${item.id}]{${item.title}}`
+				)
 				.join("+");
 			const str = `div[class="card-content category-card]>h6{分类}+(${categoryLink})`;
 			element.appendChild(
@@ -85,40 +65,57 @@
 		});
 	}
 
-	function insertTagsCard() {
+	function insertTagsCard(tagsList) {
 		return new Promise((resolve, reject) => {
-			const tagsList = [
-				{
-					url: "#",
-					title: "前端",
-				},
-				{
-					url: "#",
-					title: "JS",
-				},
-				{
-					url: "#",
-					title: "CSS",
-				},
-				{
-					url: "#",
-					title: "HTML",
-				},
-				{
-					url: "#",
-					title: "Vue",
-				},
-				{
-					url: "#",
-					title: "Node",
-				},
-			];
 			const tagsLink = tagsList
-				.map((item) => `a[href="${item.url}]{${item.title}}`)
+				.map((item) => `a[href="./tags.html?id=${item.id}]{${item.title}}`)
 				.join("+");
 			const str = `div[class="card-content tags-card]>h6{标签}+(${tagsLink})`;
 			element.appendChild(
 				".tags-card-flag",
+				element.generateDomObjArr(str)[0],
+				true
+			);
+		});
+	}
+
+	function insertArticles(articles) {
+		return new Promise((resolve, reject) => {
+			`<div class="card-container">
+      <div class="card-content">
+        <div class="img-box">
+          <img src="https://picsum.photos/1440/900/?random=1&blur=2"/>
+        </div>
+        <div class="abstract">
+          <div class="note-tips">
+            <i class="iconfont icon-time-circle"></i>
+            <h6 class="font-regular">2020-04-11</h6>
+          </div>
+          <a href="#" class="title">
+            <h2>github Issue 作为博客微型数据库的应用</h2>
+          </a>
+          <p class="content">CDN的全称是Content Delivery</p>
+          <div class="category-nav">
+            <div class="level-item">
+              <i class="iconfont icon-folder"></i>
+              <a href="#">工具教程</a>
+            </div>
+            <div class="level-item">
+              <i class="iconfont icon-tags"></i>
+              <a href="#">工具教程</a>
+              <a href="#">工具教程</a>
+              <a href="#">工具教程</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>`;
+			const imgBox = `div[class="img-box]>img[src="https://picsum.photos/1440/900/?random=${Math.random()
+				.toString()
+				.substr(3)}&blur=2"]`;
+			const noteTips = `div[class="note-tips]>(i[class="iconfont icon-time-circle]+h6[]{})`;
+			element.appendChild(
+				".person-card-flag",
 				element.generateDomObjArr(str)[0],
 				true
 			);
